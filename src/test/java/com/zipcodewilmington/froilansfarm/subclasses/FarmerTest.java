db27731d69440c16986613608d4e64f95a3e3e65
Yun <yun@zipcoders-MacBook-Pro-2.local>
2022-03-24 22:01:03 -0400

    mostly done plot

--- src/test/java/com/zipcodewilmington/froilansfarm/subclasses/FarmerTest.java
+++ src/test/java/com/zipcodewilmington/froilansfarm/subclasses/FarmerTest.java
@@ -1,6 +1,8 @@
 package com.zipcodewilmington.froilansfarm.subclasses;
 
 
+import com.zipcodewilmington.froilansfarm.collections.*;
+import com.zipcodewilmington.froilansfarm.superclasses.Vehicle;
 import org.junit.Assert;
 import org.junit.jupiter.api.Test;
 
@@ -125,12 +127,63 @@ class FarmerTest {
         froilansFarm.setOwner(froilan);
         Field field = new Field();
         CropRow cropRow = new CropRow();
-        ChickenCoop chickenHouse = new ChickenCoop();
-        Chicken chicken1 = new Chicken();
-
-
         Cornstalk cornRow1 = new Cornstalk();
         TomatoPlant tomatoRow2 = new TomatoPlant();
+        Tractor tractor1 = new Tractor();
+        Tractor tractor2 = new Tractor();
+        CropDuster cropDuster = new CropDuster();
+
+
+        Stable horseHouse1 = new Stable();
+        for(int i = 1; i <=3; i++) {
+            Horse horse = new Horse();
+            horseHouse1.add(horse);
+        }
+
+        Stable horseHouse2 = new Stable();
+        for(int i = 1; i <=3; i++) {
+            Horse horse = new Horse();
+            horseHouse2.add(horse);
+        }
+
+        Stable horseHouse3 = new Stable();
+        for(int i = 1; i <=3; i++) {
+            Horse horse = new Horse();
+            horseHouse3.add(horse);
+        }
+
+        Stable horseHouse4 = new Stable();
+            Horse horse = new Horse();
+            horseHouse4.add(horse);
+
+
+        ChickenCoop chickenHouse1 = new ChickenCoop();
+        for(int i = 1; i <=4; i++) {
+            Chicken chicken = new Chicken();
+            chickenHouse1.add(chicken);
+        }
+
+        ChickenCoop chickenHouse2 = new ChickenCoop();
+        for(int i = 1; i <=4; i++){
+            Chicken chicken = new Chicken();
+            chickenHouse2.add(chicken);
+        }
+
+        ChickenCoop chickenHouse3 = new ChickenCoop();
+        for(int i = 1; i <=4; i++){
+            Chicken chicken = new Chicken();
+            chickenHouse3.add(chicken);
+        }
+
+        ChickenCoop chickenHouse4 = new ChickenCoop();
+        for(int i = 1; i <=3; i++){
+            Chicken chicken = new Chicken();
+            chickenHouse4.add(chicken);
+        }
+
+
+
+
 
 
 
@@ -143,6 +196,14 @@ class FarmerTest {
 
 
         //
+        Assert.assertEquals(horseHouse1.size(), 3);
+        Assert.assertEquals(horseHouse2.size(), 3);
+        Assert.assertEquals(horseHouse3.size(), 3);
+        Assert.assertEquals(horseHouse4.size(), 1);
+        Assert.assertEquals(chickenHouse1.size(), 4);
+        Assert.assertEquals(chickenHouse2.size(), 4);
+        Assert.assertEquals(chickenHouse3.size(), 4);
+        Assert.assertEquals(chickenHouse4.size(), 3);
         Assert.assertEquals(cropRow.size(), 2);
         Assert.assertEquals(froilansFarm.getOwner(),froilan);
 
