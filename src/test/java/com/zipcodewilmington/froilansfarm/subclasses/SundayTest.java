9d5483a15b2bb0e6e8e32fd5b898207e4a72122e
Yun <yun@zipcoders-MacBook-Pro-2.local>
2022-03-28 00:36:05 -0400

    sunday test, working on corn fed to horse

--- src/test/java/com/zipcodewilmington/froilansfarm/subclasses/SundayTest.java
+++ src/test/java/com/zipcodewilmington/froilansfarm/subclasses/SundayTest.java
@@ -1,5 +1,8 @@
 package com.zipcodewilmington.froilansfarm.subclasses;
 
+import com.zipcodewilmington.froilansfarm.collections.Stable;
+import com.zipcodewilmington.froilansfarm.superclasses.Edible;
+import org.junit.Assert;
 import org.junit.jupiter.api.Test;
 
 public class SundayTest {
@@ -9,6 +12,44 @@ public class SundayTest {
     void workWeekTest(){
         Farmer froilan = new Farmer("Froilan");
         Farmer froilanda = new Farmer("Froilanda");
+        Horse horse1 = new Horse();
+        Horse horse2 = new Horse();
+        Stable stable1 = new Stable();
+        Stable stable2 = new Stable();
+        for(int i = 1; i <=6; i++) {
+            Cornstalk testCornstalk = new Cornstalk();
+            testCornstalk.setHarvested(true);
+            Edible actual = testCornstalk.yield();
+        }
+
+        stable1.add(horse1);
+        stable2.add(horse2);
+
+
+
+        for(int i = 1; i <=3; i++) {
+            horse1.eat(actual);
+        }
+
+        for(int i = 1; i <=3; i++) {
+            horse2.eat(actual);
+        }
+
+
+        horse1.ride(froilan);
+
+        horse2.ride(froilanda);
+
+        Assert.assertFalse(actual instanceof EarCorn);
+        Assert.assertEquals(horse1.getMountStatus(), true);
+        Assert.assertEquals(horse2.getMountStatus(), true);
+
+
+        Assert.assertTrue(stable1.contains(horse1));
+        Assert.assertTrue(stable1.contains(horse2));
+
+
+
 
 
 
